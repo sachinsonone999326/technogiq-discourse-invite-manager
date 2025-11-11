bed4ee6324a3da68035a0596ad8377c1e5c98035
sachinsonone999326 <sachin999326@gmail.com>
2025-11-11 15:44:17 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -9,35 +9,12 @@
 
 enabled_site_setting :invite_manager_enabled
 
-module ::MyPluginModule
+module ::TechnogiqDiscourseModule
   PLUGIN_NAME = "technogiq-discourse-invite-manager"
 end
 
-require_relative "lib/my_plugin_module/engine"
+require_relative "lib/technogiq_discourse_module/engine"
 
 after_initialize do
-  require_dependency "application_controller"
-  require_dependency "invite"
-
-  # ⚠️ Do not manually load jobs if located in app/jobs/scheduled/
-  # Discourse autoloads them automatically.
-  # Example correct location: app/jobs/scheduled/check_invite_expiration.rb
-
-  #
-  # --- Extend the Invite model to support metadata ---
-  #
-  class ::Invite
-    has_many :invite_metadata, dependent: :destroy
-
-    def set_metadata(key, value)
-      record = invite_metadata.find_or_initialize_by(key: key)
-      record.value = value.is_a?(Hash) ? value.to_json : value.to_s
-      record.save!
-    end
-
-    def get_metadata(key)
-      record = invite_metadata.find_by(key: key)
-      record&.value
-    end
-  end
+  # Code which should run after Rails has finished booting
 end
