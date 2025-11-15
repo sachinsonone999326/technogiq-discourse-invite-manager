7c6a22ba1c475446ce53475c308252a08a11d437
sachinsonone999326 <sachin999326@gmail.com>
2025-11-15 16:52:54 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 TechnogiqDiscourseModule::Engine.routes.draw do
-  post "/invites" => "invites#create"
+  #post "/invites" => "invites#create"
+  post "/invites" => "invite_manager#create"
   # define routes here
 end
 
-Discourse::Application.routes.draw { mount ::TechnogiqDiscourseModule::Engine, at: "my-plugin" }
+Discourse::Application.routes.draw { mount ::TechnogiqDiscourseModule::Engine, at: "technogiq-discourse-invite-manager" }
