8938c83ca7b0b1f9f184e50cc52a41c423fe6dc2
sachinsonone999326 <sachin999326@gmail.com>
2025-11-15 16:53:53 +0530

    Update engine.rb

--- lib/technogiq_discourse_module/engine.rb
+++ lib/technogiq_discourse_module/engine.rb
@@ -1,13 +1,30 @@
 # frozen_string_literal: true
 
+#module ::TechnogiqDiscourseModule
+#  class Engine < ::Rails::Engine
+#    engine_name PLUGIN_NAME
+#    isolate_namespace TechnogiqDiscourseModule
+#    config.autoload_paths << File.join(config.root, "lib")
+#    scheduled_job_dir = "#{config.root}/app/jobs/scheduled"
+#    config.to_prepare do
+#      Rails.autoloaders.main.eager_load_dir(scheduled_job_dir) if Dir.exist?(scheduled_job_dir)
+#    end
+#  end
+#end
+
+
 module ::TechnogiqDiscourseModule
   class Engine < ::Rails::Engine
-    engine_name PLUGIN_NAME
+    engine_name "technogiq_discourse_module"
     isolate_namespace TechnogiqDiscourseModule
+
     config.autoload_paths << File.join(config.root, "lib")
+
     scheduled_job_dir = "#{config.root}/app/jobs/scheduled"
     config.to_prepare do
-      Rails.autoloaders.main.eager_load_dir(scheduled_job_dir) if Dir.exist?(scheduled_job_dir)
+      if Dir.exist?(scheduled_job_dir)
+        Rails.autoloaders.main.eager_load_dir(scheduled_job_dir)
+      end
     end
   end
 end
