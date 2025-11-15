aaaea6053f6312e29c28d54d80f304d20f650697
sachinsonone999326 <sachin999326@gmail.com>
2025-11-15 16:51:20 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -14,7 +14,31 @@ module ::TechnogiqDiscourseModule
 end
 
 require_relative "lib/technogiq_discourse_module/engine"
+require_relative "app/models/invite_metadata"
 
 after_initialize do
+  require_dependency "invite"
+  #require_dependency "invite_sender"
+  class ::Invite
+    has_many :invite_metadata, dependent: :destroy, class_name: "InviteMetadatum", foreign_key: "invite_id"
+
+    def set_metadata(key, value)
+      record = invite_metadata.find_or_initialize_by(key: key)
+      record.value = value.is_a?(Hash) ? value.to_json : value.to_s
+      record.save!
+    end
+
+    def get_metadata(key)
+      record = invite_metadata.find_by(key: key)
+      record&.value
+    end
+  end
   # Code which should run after Rails has finished booting
+  #Discourse::Application.routes.append do
+    #mount ::TechnogiqDiscourseModule::Engine, at: "/technogiq-discourse-invite-manager"
+  #end
 end
+
+#Discourse::Application.routes.append do
+#  mount ::TechnogiqDiscourseModule::Engine, at: "/technogiq_discourse_module"
+#end
