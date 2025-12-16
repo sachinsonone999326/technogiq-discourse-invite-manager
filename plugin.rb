16f51e622d4319a72124bb283cad0b90c47c6179
sachinsonone999326 <sachin999326@gmail.com>
2025-12-16 18:54:08 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -45,6 +45,8 @@ after_initialize do
   #add_admin_route "technogiq-discourse-invite-manager.title", "technogiq-discourse-invite-manager"
   add_admin_route "technogiq_invite_manager.title", "technogiq-invite-manager", use_new_show_route: true
   Discourse::Application.routes.append do
+    get "/admin/plugins/technogiq-invite-manager" => "technogiq_discourse_module/invite_manager#index",
+        :constraints => StaffConstraint.new
     #get '/admin/plugins/technogiq-discourse-invite-manager' => 'admin/plugins#index', constraints: StaffConstraint.new
     #get '/admin/plugins/technogiq-invite-manager' => 'admin/plugins#index'
     namespace :admin do
