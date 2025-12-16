3df503369d16c671d86004e9295e378a08f652b5
sachinsonone999326 <sachin999326@gmail.com>
2025-12-16 17:43:57 +0530

    Delete assets/javascripts/discourse/admin directory

--- assets/javascripts/discourse/admin/routes/technogiq-invites.js
+++
@@ -1,4 +0,0 @@
-// assets/javascripts/discourse/admin/routes/technogiq-invites.js
-import AdminRoute from "discourse/routes/admin";
-
-export default class TechnogiqInvitesRoute extends AdminRoute {}
