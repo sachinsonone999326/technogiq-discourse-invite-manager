e9c377a3237e39f0df8ac827e872f8ddf7b29c5e
sachinsonone999326 <sachin999326@gmail.com>
2025-12-18 20:19:36 +0530

    Delete admin/assets/javascripts/discourse/routes/admin-plugins/show/discourse-technogiq-discourse-invite-manager.js

--- admin/assets/javascripts/discourse/routes/admin-plugins/show/discourse-technogiq-discourse-invite-manager.js
+++
@@ -1,7 +0,0 @@
-import DiscourseRoute from "discourse/routes/discourse";
-
-export default class TechnogiqDiscourseInviteManagerTechnogiqDiscourseInviteManagerRoute extends DiscourseRoute {
-  model() {
-    return this.store.findAll("technogiq-discourse-invite-manager");
-  }
-}
