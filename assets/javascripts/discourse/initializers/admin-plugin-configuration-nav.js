a98dce8c4c36a3887b76408dc8575a5a62b9d25d
sachinsonone999326 <sachin999326@gmail.com>
2025-12-19 19:07:26 +0530

    Create admin-plugin-configuration-nav.js

---
+++ assets/javascripts/discourse/initializers/admin-plugin-configuration-nav.js
@@ -0,0 +1,23 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "technogiq-discourse-invite-manager-admin-plugin-configuration-nav",
+
+  initialize(container) {
+    const currentUser = container.lookup("service:current-user");
+    if (!currentUser || !currentUser.admin) {
+      return;
+    }
+
+    withPluginApi((api) => {
+      api.addAdminPluginConfigurationNav("technogiq-discourse-invite-manager", [
+        {
+          label: "technogiq_invite_manager.title",
+          route: "adminPlugins.show.technogiq-discourse-invite-manager-invites",
+          description: "technogiq_invite_manager.title",
+        },
+        
+      ]);
+    });
+  },
+};
