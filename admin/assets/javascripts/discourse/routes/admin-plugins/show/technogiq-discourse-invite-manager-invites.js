4d08bd9a8956b42e0ead18891fa939626d08e36b
sachinsonone999326 <sachin999326@gmail.com>
2025-12-22 16:39:03 +0530

    Update technogiq-discourse-invite-manager-invites.js

--- admin/assets/javascripts/discourse/routes/admin-plugins/show/technogiq-discourse-invite-manager-invites.js
+++ admin/assets/javascripts/discourse/routes/admin-plugins/show/technogiq-discourse-invite-manager-invites.js
@@ -2,6 +2,6 @@ import DiscourseRoute from "discourse/routes/discourse";
 
 export default class TechnogiqDiscourseInviteManagerInvitesRoute extends DiscourseRoute {
   model() {
-    return this.store.findAll("invites");
+    return this.store.findAll("invite");
   }
 }
