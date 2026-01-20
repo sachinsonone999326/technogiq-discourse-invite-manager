454836df36ae15d5169b2ba0695bb62d9b87b7d2
sachinsonone999326 <sachin999326@gmail.com>
2026-01-20 18:09:19 +0530

    Update 20251105124500_create_invite_metadata.rb

--- db/migrate/20251105124500_create_invite_metadata.rb
+++ db/migrate/20251105124500_create_invite_metadata.rb
@@ -5,9 +5,11 @@ class CreateInviteMetadata < ActiveRecord::Migration[7.0]
   def change
     create_table :invite_metadata do |t|
       t.integer :invite_id, null: false
-      t.string :key, null: false
-      t.text :value
+      t.jsonb :metadata, null: false, default: {}
+      t.boolean :is_expiry_date, null: false, default: false
       t.datetime :expiration_date
+      t.string :plan_type
+      t.integer :membership_duration_value
       t.timestamps
     end
 
