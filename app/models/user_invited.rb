1ddafddc699890f70467e972be609239e09a48ca
sachinsonone999326 <sachin999326@gmail.com>
2026-01-21 15:41:02 +0530

    Update user_invited.rb

--- app/models/user_invited.rb
+++ app/models/user_invited.rb
@@ -4,4 +4,9 @@
 class UserInvited < ActiveRecord::Base
   belongs_to :user
   belongs_to :invite
+
+  validates :invite_id, presence: true
+  validates :metadata, presence: true
+  validates :user_id, presence: true
+  
 end
