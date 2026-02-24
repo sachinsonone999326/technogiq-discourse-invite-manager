ab96606b34c5e6cf582b6d4361bd0ee5516deac6
sachinsonone999326 <sachin999326@gmail.com>
2026-02-24 17:02:10 +0530

    Delete db/migrate/20260320124500_add_ref_invite_metadata_structure.rb

--- db/migrate/20260320124500_add_ref_invite_metadata_structure.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddRefInviteMetadataStructure < ActiveRecord::Migration[7.0]
-  def change
-    #  Add new columns
-    add_column :invite_metadata, :uniqueid, :string
-  end
-end
