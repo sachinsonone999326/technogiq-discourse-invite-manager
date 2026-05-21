21dc2f5a4b149030dd584dffb5f31c403efb03f3
sachinsonone999326 <sachin999326@gmail.com>
2026-05-21 18:09:57 +0530

    Update check_invite_expiration.rb

--- app/jobs/scheduled/check_invite_expiration.rb
+++ app/jobs/scheduled/check_invite_expiration.rb
@@ -1,94 +1,126 @@
 # frozen_string_literal: true
-#
-
-#module Jobs
-#  class CheckInviteExpiration < ::Jobs::Scheduled
-#    every 1.day
-
-#    def execute(args)
-#      return true #if !SiteSetting.invite_manager_enabled
-#    end
-#  end
-#end
-
 
 module Jobs
   class CheckInviteExpiration < ::Jobs::Scheduled
+
     every 1.day
 
     def execute(args)
-      Rails.logger.info("InviteManager: Running expiration cron job")
 
-      InviteMetadata.where(key: "expiration_date").find_each do |meta|
+      Rails.logger.info(
+        "InviteManager: Running expiration cron job"
+      )
+
+      UserInviteum
+        .where.not(expiration_date: nil)
+        .where(
+          "expiration_date BETWEEN ? AND ?",
+          Date.today - 1.day,
+          Date.today + 5.days
+        )
+        .find_each do |invite_record|
+
         begin
-          expiration = parse_date(meta.value)
-          next if expiration.nil?
 
-          invite = Invite.find_by(id: meta.invite_id)
-          next if invite.nil? || !invite.redeemed?
+          user = User.find_by(
+            id: invite_record.user_id
+          )
 
-          #user = invite.user
-          #user = invite.redeemed_users.first
-          #next if user.nil?
-          invited_user = InvitedUser.find_by(invite_id: invite.id)
-          user = invited_user&.user
           next if user.nil?
 
-          # ----- 5 DAY WARNING -----
-          days_left = (expiration - Date.today).to_i
+          # Skip already deactivated users
+          next if user.suspended?
 
-          #if days_left == 5
-          #  send_warning_notification(user, expiration)
-          #  Rails.logger.info(
-          #    "InviteManager: Sent 5-day warning to #{user.username} (expires: #{expiration})"
-          #  )
-          #end
+          expiration =
+            invite_record.expiration_date.to_date
 
-          if days_left <= 5 && days_left > 0
-            send_warning_notification(user, expiration, days_left)
-            Rails.logger.info(
-              "InviteManager: Sent daily warning to #{user.username}, #{days_left} days left"
+          days_left =
+            (expiration - Date.today).to_i
+
+          # --------------------------------
+          # SEND WARNING NOTIFICATION
+          # --------------------------------
+
+          if days_left <= 5 &&
+             days_left > 0
+
+            send_warning_notification(
+              user,
+              expiration,
+              days_left
             )
+
+            Rails.logger.info(
+              "InviteManager: Warning sent to #{user.username}, #{days_left} days left"
+            )
+
           end
 
-          # ----- EXPIRATION CHECK -----
-          if expiration < Date.today && user.active?
-            user.deactivate("Invite expired")
-            Rails.logger.info(
-              "InviteManager: Deactivated user #{user.username} due to expired invite (expired on #{expiration})"
+          # --------------------------------
+          # DEACTIVATE USER
+          # --------------------------------
+
+          if expiration < Date.today
+          
+            user.deactivate(
+              "Invite expired"
             )
+
+
+            user.update_columns(
+                suspended_till: 100.years.from_now
+            )
+            
+            #user.update_columns(
+            #    suspend_reason: "Your membership has expired. Please contact admin for renewal."
+            #)
+
+            UserAuthToken.where(
+               user_id: user.id
+            ).each(&:destroy!)
+
+            Rails.logger.info(
+                "InviteManager: Suspended #{user.username}"
+            )
+
           end
 
         rescue => e
-          Rails.logger.error("InviteManager Cron Error: #{e.message}")
+
+          Rails.logger.error(
+            "InviteManager Cron Error: #{e.message}"
+          )
+
         end
       end
     end
 
     private
 
-    def parse_date(value)
-      Date.parse(value) rescue nil
-    end
+    def send_warning_notification(
+      user,
+      expiration_date,
+      days_left
+    )
 
-    def send_warning_notification(user, expiration_date, days_left)
       message = <<~MSG
-        Your account access will expire in **#{days_left} days** (on **#{expiration_date}**)
+        Your account access will expire in **#{days_left} days**
+        (on **#{expiration_date}**)
         because your invitation has a time limit.
 
-        If you believe this is a mistake or you need an extension,
+        If you believe this is a mistake or need extension,
         please contact the admin.
       MSG
 
       PostCreator.create!(
         Discourse.system_user,
-        #target_user: user,
         target_usernames: user.username,
         archetype: Archetype.private_message,
         subtype: TopicSubtype.system_message,
         title: "Your account will expire soon",
         raw: message
       )
+
     end
   end
 end
